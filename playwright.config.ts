000d57e071f84c390caef46ce1beaee72125e73d
nsmle <fikiproductionofficial@gmail.com>
2025-05-21 18:30:53 +0700

    refactor: move e2e test cases to tests/e2e directory

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ const gitHubActionReporterOptions: GitHubActionOptions = {
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-	testDir: "./tests",
+	testDir: "./tests/e2e",
 	/* Run tests in files in parallel */
 	fullyParallel: true,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
